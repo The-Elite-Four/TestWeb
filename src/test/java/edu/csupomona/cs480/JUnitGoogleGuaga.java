84177d791ea6a2dc942f12acadd97ca29c456f47
Nawaf Alsufiani <nawafh1986@hotmail.com>
2016-07-11 21:59:47 -0700

    The Google guaga was added again
    this is for the assignment 4 which didn't work before for the error..
    Google guava added with the pox.xml update.
    Nawaf

---
+++ src/test/java/edu/csupomona/cs480/JUnitGoogleGuaga.java
@@ -0,0 +1,19 @@
+package edu.csupomona.cs480;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.csupomona.cs480.controller.WebController;
+
+public class JUnitGoogleGuaga {
+
+	@Test
+	public void test() {
+
+		WebController controller = new WebController();
+ 
+  
+	}
+
+}
